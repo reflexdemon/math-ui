10ff6eb8160d7de63fc6ac1733cf72126b0e65dc
Venkateswara VP <venkateswara.vp@gmail.com>
2026-01-07 18:04:03 -0500

    Add random content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,10 @@
 
 <template>
   <main>
-    <h1>Home View</h1>
+    <h1>Home Page.</h1>
+    <p>
+      Welcome to the home page of our application. This is where you can find the latest updates and
+      information.
+    </p>
   </main>
 </template>
